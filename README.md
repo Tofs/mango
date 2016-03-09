1c053a40abe9c0511aed12c4f8b9770f8d4d024f
Anton Einarson <anton.einarson@gmail.com>
2016-03-09 23:13:11 +0100

    dummy ui in place.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 The todo manager of the future.
 
 # Usage
+## keyword
+add : add an item to the database
+list : list all items in the database
+tag : add a tag to an item
+remove : remove a item
+init : creates a database
+status : sets the status
---
+++ gui.py
@@ -0,0 +1,42 @@
+import pygtk
+import gtk
+
+class helloworld:
+    def helloworld(self, wiget, data=None):
+        print "Hello world from mango brain!"
+
+    def delete_event(self, widget, event, data=None):
+        print "Delete event"
+        return False
+
+    def destroy(self, widget, data=None):
+        gtk.main_quit()
+
+    def __init__(self):
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.connect("delete_event", self.delete_event)
+        self.window.connect("destroy", self.destroy)
+
+        self.window.set_border_width(10)
+        self.button1 = gtk.Button("Hello world! 1")
+        self.button2 = gtk.Button("Hello world! 2")
+        self.button1.connect("clicked", self.helloworld, None)
+        self.button2.connect("clicked", self.helloworld, None)
+        self.box1 = gtk.HBox(False, 0)
+        self.box1.pack_start(self.button1, True,True, 0)
+        self.box1.pack_start(self.button2, True,True, 0)
+        self.button1.connect_object("clicked",  gtk.Widget.destroy, self.window)
+
+        self.window.add(self.box1)
+        self.button1.show()
+        self.button2.show()
+        self.box1.show()
+        self.window.show()
+
+    def main(self):
+        gtk.main()
+
+
+if __name__ == "__main__":
+    hello = helloworld()
+    hello.main()
