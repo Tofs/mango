1063873bd091e94c34c911683eaea9597abb596f
Anton Einarson <anton.einarson@gmail.com>
2016-03-14 20:59:43 +0100

    moved the title to the window removed table code couse the table widget is depricated.

--- gui.py
+++ gui.py
@@ -12,32 +12,22 @@ class helloworld:
     def destroy(self, widget, data=None):
         gtk.main_quit()
 
+    def createTable(self):
+        return None
+
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title("Mango Mapper")
         self.window.connect("delete_event", self.delete_event)
         self.window.connect("destroy", self.destroy)
         self.window.set_border_width(10)
         self.window.show()
 
-
-
-
-
-
-
         self.verticalbox = gtk.VBox(False, 0)
         self.window.add(self.verticalbox)
         self.verticalbox.show()
 
-
-        self.title = gtk.Label("Mango mapper")
-        self.verticalbox.pack_start(self.title, True, True, 0)
-        self.title.show()
-
-
-        self.mainTable = gtk.Table(rows=1 , columns=1)
-        self.verticalbox.pack_start(self.mainTable, True, True, 0)
-        self.mainTable.show()
+        self.table = self.createTable()
 
 
 
